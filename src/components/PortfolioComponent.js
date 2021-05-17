fe5dc93fe198ff1d077da0baad295f7b8b1c26a9
Consolatrice-tech <bconso7@gmail.com>
2021-05-17 14:49:24 +0200

    card

--- src/components/PortfolioComponent.js
+++ src/components/PortfolioComponent.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './index.css';
+import {Card} from 'antd';
 
 const PortfolioComponent=()=>{
     return (
-        <div className="Portfolio-container">
+        <Card className="Portfolio-container">
         <h1> Portfolio </h1>
         <hr/> 
         <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. 
             Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
             Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
+        </Card>
 
     )};
     export default PortfolioComponent;
