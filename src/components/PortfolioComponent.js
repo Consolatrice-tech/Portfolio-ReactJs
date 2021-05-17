195f840b4ebea109d98e66072e31197c7a6fb8bb
Consolatrice-tech <73180096+gentcode1@users.noreply.github.com>
2021-05-17 14:43:03 +0200

    Changes div

--- src/components/PortfolioComponent.js
+++ src/components/PortfolioComponent.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './index.css';
-import {Card} from 'antd';
 
 const PortfolioComponent=()=>{
     return (
-        <Card className="Portfolio-container">
+        <div className="Portfolio-container">
         <h1> Portfolio </h1>
         <hr/> 
         <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. 
             Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
             Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </Card>
+        </div>
 
     )};
     export default PortfolioComponent;
